ac8414ac8aa79731b0ff7e3d28198ed4f5b5ac99
KevGrav <kgravell.prsvr@gmail.com>
2023-09-19 12:20:28 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+let myFirstName = "Kevin";
+let myLastName = "Gravell";
+let myBestFriend = "Richard";
+let mySentence = "My name is " + myFirstName + " " + myLastName + ' and my best friend is' + myBestFriend;
+let myFavoriteNum = 9;
+let myMentalAge = 55;
+let myHeight = 71;
+let myFavoriteYear = 1997;
+let mySum = 3 + 1918 + 24 + 1;
+let myProduct = 232 * 43 / 2 + 4;
+let myHeightAndAge = myMentalAge + myHeight;
+
+
+
+
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myBestFriend === "undefined") {
+  myBestFriend = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myFavoriteNum === "undefined") {
+  myFavoriteNum = undefined;
+}
+
+if (typeof myMentalAge === "undefined") {
+  myMentalAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myBestFriend,
+  mySentence,
+  myFavoriteNum,
+  myMentalAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
